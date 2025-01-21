dbd43af212bb6f797d95c89e35e381ac6425d771
InTaewoo <oilehot0910@naver.com>
2025-01-21 15:27:29 +0900

    fixed jobs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "vprofile-eks"
 
   cidr = "172.20.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3) # 총3개의 가용성 존을 갖음
 
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
